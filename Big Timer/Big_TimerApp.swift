8f1d7aeef4a64365af2a6afc0b470f90b80867fb
Donghyun Lee <leedonghyun022@gmail.com>
2026-01-17 18:27:08 +0900

    Initial Commit

---
+++ Big Timer/Big_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Big_TimerApp.swift
+//  Big Timer
+//
+//  Created by Donghyun Lee on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Big_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
